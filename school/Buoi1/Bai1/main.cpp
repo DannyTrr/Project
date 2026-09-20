d41af45e826d6b4e815d9ee0d31b9e95eae8c08b
DannyTrr <Tuandattran595@gmail.com>
2026-09-20 15:19:11 +0700

    feat : hoan thanh baii va luu do

---
+++ school/Buoi1/Bai1/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cout <<"Nhap n:";
+cin >> n;
+if (n <0)
+cout <<" n la so am "<<endl;
+else
+if (n==0)
+cout <<" n la so 0 "<<endl;
+else
+cout << "n la so duong" <<endl;
+
+}
