78c72224972579170d11e4a21b697ef114364606
DannyTrr <Tuandattran595@gmail.com>
2026-09-20 15:47:06 +0700

    feat: hoan thanh bai 2 buoi 1

---
+++ school/Buoi1/Bai2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Nhap n:";
+    cin >> n;
+    if (n %2 == 0)
+    cout << "n la so chan " << endl;
+    else
+    cout << "n la so le " << endl;
+return 0;
+}
